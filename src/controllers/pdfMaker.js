96c9aef88042ad748e78fc75ab43a953e631f2ba
danielnasc <danielnasc15987@gmail.com>
2021-07-07 18:56:15 -0300

    / BRUUHH!

--- src/controllers/pdfMaker.js
+++ src/controllers/pdfMaker.js
@@ -5,7 +5,7 @@ module.exports = {
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
 
-        await page.goto(`http://localhost:3000/result${path}`,{
+        await page.goto(`http://localhost:3000/result/${path}`,{
             waitUntil: "networkidle0"
         })
 
