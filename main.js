8c7c2d3413703431aab84e857762ee10494a5a12
danielnasc <danielnasc15987@gmail.com>
2021-07-06 21:02:40 -0300

    Beta 1.0

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const {BrowserWindow, app} = require('electron');
+const {BrowserWindow, app, globalShortcut} = require('electron');
 require('./src/app');
 
 let mainWindow = null
 
 function main(){
-    mainWindow = new BrowserWindow()
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
     mainWindow.loadURL('http://localhost:3000')
+    mainWindow.removeMenu()
     mainWindow.on('close',event=>{
         mainWindow = null
+    })
+
+    globalShortcut.register('Escape', ()=>{
+        app.relaunch()
+        app.exit(0)
     })
 }
 
