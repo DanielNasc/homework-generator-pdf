176065f04f815a6ec0cf2123feb642c51e970726
danielnasc <danielnasc15987@gmail.com>
2021-07-11 15:53:27 -0300

    puppeteer error

--- src/services/pdfMaker.js
+++ src/services/pdfMaker.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer')
 
 module.exports = {
     async makePDF(path){
-        const browser = await puppeteer.launch()
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox','--disable-setuid-sandbox']
+          })
+          
         const page = await browser.newPage()
 
         await page.goto(`http://localhost:3000/result/${path}`,{
