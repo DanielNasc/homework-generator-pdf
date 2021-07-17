6e1f882f2695d3e792df42e019e77cfa1eaea3ef
danielnasc <danielnasc15987@gmail.com>
2021-07-17 18:30:49 -0300

    H12 Error Heroku page

--- src/app.js
+++ src/app.js
@@ -6,20 +6,20 @@ const path = require('path')
 const resultController = require('./controllers/resultController')
 
 const PORT = process.env.PORT || 3000
-const htmlPath = path.join(__dirname,'../views/index.html')
+const htmlPath = path.join(__dirname,'../views/')
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(cookie_parser())
 
-app.get('/', (req,res) => res.sendFile(htmlPath))
-app.post('/', resultController.save)
+app.get('/', (req,res) => res.sendFile(htmlPath + "index.html"))
+app.get('/http-error', (req,res) => res.sendFile(htmlPath + "http-error.html"))
 app.get('/result/:path', resultController.render)
 app.get('/ops', resultController.erro)
-app.use((req,res) => {
-    req.setTimeout
-    res.status(404).send('<h1>404</h1>')})
 
-const server = app.listen(PORT)
-server.setTimeout(500000)
+app.post('/', resultController.save)
+
+app.use((req,res) => {res.status(404).send('<h1>404</h1>')})
+
+app.listen(PORT)
